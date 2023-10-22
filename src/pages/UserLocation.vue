69c036bcfb251e2089b64205d50289950fede956
Phoebe <phyuhala@gmail.com>
2023-10-21 22:14:53 -0700

    get user location

---
+++ src/pages/UserLocation.vue
@@ -0,0 +1,48 @@
+<template>
+  <section  class="ui two column centered grid">
+    <div class="column">
+        <form action="" class="ui segment large form">
+            <div class="ui message red"> </div>
+            <div class="ui segment"> 
+                <div class="field">
+                    <div class="ui right icon input large">
+                        <input type="text" placeholder="Enter">
+                        <button class="ui button" @click="locatorButtonPressed">Go</button>
+                    </div>
+                </div>
+                <button class="ui button">Go</button>
+            </div>
+        </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default{
+    methods: {
+        locatorButtonPressed(){
+            console.log('hi')
+            if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition(
+                    position =>{
+                        console.log(position.coords.latitude)
+                        console.log(position.coords.longitude)
+                    },
+                    error =>{
+                        console.log(error.message)
+                    }
+                )  
+            }else{
+                console.log("Your browser does not support geolocation API")
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.ui.button{
+    background-color: #ff5e5f;
+    color: #fff;
+} 
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import UserLocation from '@/pages/UserLocation'
 
 Vue.use(Router)
 
@@ -8,8 +8,9 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: UserLocation
     }
   ]
 })
+
+
